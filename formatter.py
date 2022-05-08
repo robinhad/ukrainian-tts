2079a962113322c0dc1fcc3d52be13a80de2d34f
Yurii Paniv <mr.robinhad@gmail.com>
2022-05-08 21:00:44 +0300

    Add stress in numbers

--- formatter.py
+++ formatter.py
@@ -3,16 +3,16 @@ def preprocess_text(text):
     text = text.replace("`", "'")
     text = text.replace("ʼ", "'")
     # numbers
-    text = text.replace("1", "один ")
-    text = text.replace("2", "два ")
-    text = text.replace("3", "три ")
-    text = text.replace("4", "чотири ")
-    text = text.replace("5", "п'ять ")
-    text = text.replace("6", "шість ")
-    text = text.replace("7", "сім ")
-    text = text.replace("8", "вісім ")
-    text = text.replace("9", "дев'ять ")
-    text = text.replace("0", "нуль ")
+    text = text.replace("1", "од+ин ")
+    text = text.replace("2", "дв+а ")
+    text = text.replace("3", "тр+и ")
+    text = text.replace("4", "чот+ири ")
+    text = text.replace("5", "п'+ять ")
+    text = text.replace("6", "ш+ість ")
+    text = text.replace("7", "с+ім ")
+    text = text.replace("8", "в+ісім ")
+    text = text.replace("9", "д+ев'ять ")
+    text = text.replace("0", "н+уль ")
     # speak english alphabet using brute force transliteration
     english = {
         "a": "а",
