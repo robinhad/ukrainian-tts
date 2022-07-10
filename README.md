8812d277ddcd5415d96d23ae450eeb9f5754fae9
Yurii Paniv <mr.robinhad@gmail.com>
2022-07-10 22:26:18 +0300

    Add female voice in multispeaker model

--- README.md
+++ README.md
@@ -11,14 +11,20 @@ pinned: false
 # Ukrainian TTS 📢🤖
 Ukrainian TTS (text-to-speech) using Coqui TTS.
 
-Trained on [M-AILABS Ukrainian dataset](https://www.caito.de/2019/01/the-m-ailabs-speech-dataset/).  
-
 Link to online demo -> [https://huggingface.co/spaces/robinhad/ukrainian-tts](https://huggingface.co/spaces/robinhad/ukrainian-tts)
+
+Code is licensed under `MIT License`, models are under `GNU GPL v3 License`. 
 # Support
 If you like my work, please support -> ![mono](https://www.monobank.ua/favicon.ico) [SUPPORT LINK](https://send.monobank.ua/jar/48iHq4xAXm)
 # Example
 
-https://user-images.githubusercontent.com/5759207/167310298-2f384cb0-ebef-4a92-931b-3ff6fac6c99c.mp4
+`Mykyta (male)`:
+
+https://user-images.githubusercontent.com/5759207/178158485-29a5d496-7eeb-4938-8ea7-c345bc9fed57.mp4
+
+`Olena (female)`:
+
+https://user-images.githubusercontent.com/5759207/178158492-8504080e-2f13-43f1-83f0-489b1f9cd66b.mp4
 
 # How to use :
 1. `pip install -r requirements.txt`.
@@ -26,13 +32,13 @@ https://user-images.githubusercontent.com/5759207/167310298-2f384cb0-ebef-4a92-9
 3. Launch as one-time command:  
 ```
 tts --text "Text for TTS" \
-    --model_path path/to/model.pth.tar \
+    --model_path path/to/model.pth \
     --config_path path/to/config.json \
     --out_path folder/to/save/output.wav
 ```
 or alternatively launch web server using:
 ```
-tts-server --model_path path/to/model.pth.tar \
+tts-server --model_path path/to/model.pth \
     --config_path path/to/config.json
 ```
 
--- app.py
+++ app.py
@@ -10,55 +10,75 @@ from datetime import datetime
 from enum import Enum
 import torch
 
+
 class StressOption(Enum):
     ManualStress = "Наголоси вручну"
     AutomaticStress = "Автоматичні наголоси (Beta)"
+
+
+class VoiceOption(Enum):
+    MaleVoice = "Микита (чоловічий)"
+    FemaleVoice = "Олена (жіночий)"
 
 
 def download(url, file_name):
     if not exists(file_name):
         print(f"Downloading {file_name}")
         r = requests.get(url, allow_redirects=True)
-        with open(file_name, 'wb') as file:
+        with open(file_name, "wb") as file:
             file.write(r.content)
     else:
         print(f"Found {file_name}. Skipping download...")
 
 
 print("downloading uk/mykyta/vits-tts")
-release_number = "v2.0.0-beta"
+release_number = "v2.0.0"
 model_link = f"https://github.com/robinhad/ukrainian-tts/releases/download/{release_number}/model-inference.pth"
 config_link = f"https://github.com/robinhad/ukrainian-tts/releases/download/{release_number}/config.json"
+speakers_link = f"https://github.com/robinhad/ukrainian-tts/releases/download/{release_number}/speakers.pth"
 
 model_path = "model.pth"
 config_path = "config.json"
+speakers_path = "speakers.pth"
 
 download(model_link, model_path)
 download(config_link, config_path)
+download(speakers_link, speakers_path)
 
-badge = "https://visitor-badge-reloaded.herokuapp.com/badge?page_id=robinhad.ukrainian-tts"
+badge = (
+    "https://visitor-badge-reloaded.herokuapp.com/badge?page_id=robinhad.ukrainian-tts"
+)
 
 synthesizer = Synthesizer(
-    model_path, config_path, None, None, None,
+    model_path,
+    config_path,
+    speakers_path,
+    None,
+    None,
 )
 
 if synthesizer is None:
     raise NameError("model not found")
 
-def tts(text: str, stress: str):
+
+def tts(text: str, voice: str, stress: str):
     print("============================")
     print("Original text:", text)
+    print("Voice", voice)
     print("Stress:", stress)
     print("Time:", datetime.utcnow())
     autostress = True if stress == StressOption.AutomaticStress.value else False
+    speaker_name = "male1" if voice == VoiceOption.MaleVoice.value else "female3"
     text = preprocess_text(text, autostress)
     text_limit = 1200
-    text = text if len(text) < text_limit else text[0:text_limit] # mitigate crashes on hf space
+    text = (
+        text if len(text) < text_limit else text[0:text_limit]
+    )  # mitigate crashes on hf space
     print("Converted:", text)
 
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as fp:
         with torch.no_grad():
-            wavs = synthesizer.tts(text)
+            wavs = synthesizer.tts(text, speaker_name=speaker_name)
             synthesizer.save_wav(wavs, fp)
         return fp.name, text
 
@@ -71,22 +91,30 @@ iface = gr.Interface(
             default="Введ+іть, б+удь л+аска, сво+є р+ечення.",
         ),
         gr.inputs.Radio(
-            label="Опції",
+            label="Голос",
+            choices=[option.value for option in VoiceOption],
+        ),
+        gr.inputs.Radio(
+            label="Наголоси",
             choices=[option.value for option in StressOption],
         ),
     ],
-    outputs=[gr.outputs.Audio(label="Output"), gr.outputs.Textbox(label="Наголошений текст")],
+    outputs=[
+        gr.outputs.Audio(label="Output"),
+        gr.outputs.Textbox(label="Наголошений текст"),
+    ],
     title="🐸💬🇺🇦 - Coqui TTS",
     theme="huggingface",
     description="Україномовний🇺🇦 TTS за допомогою Coqui TTS (для наголосу використовуйте + перед голосною)",
-    article="Якщо вам подобається, підтримайте за посиланням: [SUPPORT LINK](https://send.monobank.ua/jar/48iHq4xAXm),  " +
-    "Github: [https://github.com/robinhad/ukrainian-tts](https://github.com/robinhad/ukrainian-tts)" + 
-    f"<center><img src=\"{badge}\" alt=\"visitors badge\"/></center>",
+    article="Якщо вам подобається, підтримайте за посиланням: [SUPPORT LINK](https://send.monobank.ua/jar/48iHq4xAXm),  "
+    + "Github: [https://github.com/robinhad/ukrainian-tts](https://github.com/robinhad/ukrainian-tts)"
+    + f'<center><img src="{badge}" alt="visitors badge"/></center>',
     examples=[
-        ["Введ+іть, б+удь л+аска, сво+є р+ечення.", StressOption.ManualStress.value],
-        ["Введіть, будь ласка, своє речення.", StressOption.ManualStress.value],
-        ["Привіт, як тебе звати?", StressOption.AutomaticStress.value],
-        ["Договір підписано 4 квітня 1949 року.", StressOption.AutomaticStress.value],
-    ]
+        ["Введ+іть, б+удь л+аска, сво+є р+ечення.", VoiceOption.FemaleVoice.value, StressOption.ManualStress.value],
+        ["Введ+іть, б+удь л+аска, сво+є р+ечення.", VoiceOption.MaleVoice.value, StressOption.ManualStress.value],
+        ["Введіть, будь ласка, своє речення.", VoiceOption.MaleVoice.value, StressOption.ManualStress.value],
+        ["Привіт, як тебе звати?", VoiceOption.FemaleVoice.value, StressOption.AutomaticStress.value],
+        ["Договір підписано 4 квітня 1949 року.", VoiceOption.FemaleVoice.value, StressOption.AutomaticStress.value],
+    ],
 )
 iface.launch(enable_queue=True, prevent_thread_lock=True)
