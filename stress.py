de21232afccc811fadc4b8d81d1518cfb0ae248b
Yurii Paniv <mr.robinhad@gmail.com>
2022-08-01 01:55:37 +0300

    Add autostress to single vowel words

--- stress.py
+++ stress.py
@@ -1,15 +1,24 @@
+from typing import List
 import numpy as np
 from ukrainian_word_stress import Stressifier, StressSymbol
 
 stressify = Stressifier(stress_symbol=StressSymbol.CombiningAcuteAccent)
 
+vowels = "аеєиіїоуюя"
+consonants = "бвгґджзйклмнпрстфхцчшщь"
+special = "'"
+alphabet = vowels + consonants + special
+
 
 def stress_dict(sentence: str):
-    stressed = stressify(sentence.replace("+", "")).replace(StressSymbol.CombiningAcuteAccent, "+")
+    stressed = stressify(sentence.replace("+", "")).replace(
+        StressSymbol.CombiningAcuteAccent, "+"
+    )
     new_stressed = ""
     start = 0
     last = 0
 
+    # shift stress symbol by one "при+віт" -> "пр+ивіт"
     while True:
         plus_position = stressed.find("+", start)
         if plus_position != -1:
@@ -34,7 +43,21 @@ def sentence_to_stress(sentence: str, stress_function=stress_dict) -> str:
 
     # add stress before vowel
     new_stressed = stress_function(sentence)
-    
+
+    # stress single vowel words
+    new_list: List[str] = new_stressed.split(" ")
+    for word_index in range(0, len(new_list)):
+        element = new_list[word_index]
+        vowels_in_words = list(map(lambda letter: letter in vowels, element.lower()))
+        if "+" in element:
+            continue
+        if vowels_in_words.count(True) == 0:
+            continue
+        elif vowels_in_words.count(True) == 1:
+            vowel_index = vowels_in_words.index(True)
+            new_list[word_index] = element[0:vowel_index] + "+" + element[vowel_index::]
+    new_stressed = " ".join(new_list)
+
     # replace already stressed words
     if len(all_stresses) > 0:
         words = new_stressed.split(" ")
