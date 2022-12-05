9ddc3f7f5b587ca7ac8866b203477a6b2fa14029
Yurii Paniv <mr.robinhad@gmail.com>
2022-12-05 22:18:31 +0200

    Add TTS tests

---
+++ tests/test_tts.py
@@ -0,0 +1,10 @@
+from ukrainian_tts.tts import TTS, Voices, Stress
+from io import BytesIO
+
+def test_tts():
+    tts = TTS(use_cuda=False)
+    file = BytesIO()
+    _, text = tts.tts("Привіт", Voices.Dmytro.value, Stress.Dictionary.value, file)
+    file.seek(0)
+    assert text == "Прив+іт"
+    assert file.getbuffer().nbytes > 1000 # check that file was generated
