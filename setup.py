8629ef257ceba8f72ff6ee9d6e3e7924d5e31f4b
Serhii Shevchuck <serh007@gmail.com>
2024-04-25 22:49:30 +0300

    #42 Fix ukrainian-word-stress version ins setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
         "espnet==202301",
         "typeguard<3",
         "num2words @ git+https://github.com/savoirfairelinux/num2words.git@3e39091d052829fc9e65c18176ce7b7ff6169772",
-        "ukrainian-word-stress==1.0.2",
+        "ukrainian-word-stress==1.1.0",
         "ukrainian_accentor @ git+https://github.com/egorsmkv/ukrainian-accentor.git@5b7971c4e135e3ff3283336962e63fc0b1c80f4c",
-        "stanza<1.6",  # fix for ukrainian-word-stress
+        "stanza==1.7",  #for ukrainian-word-stress
     ],
 )
